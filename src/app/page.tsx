9106db3f8bea2ecd46b2a27f87bb7cca0a264c72
Tushar Dahiya <shreyashsam@gmail.com>
2024-06-12 21:52:41 +0530

    this a really awesome, nothing is happening when clicking on the button but the request is going to the server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import "server-only";
 import Link from "next/link";
 
 import { CreatePost } from "~/app/_components/create-post";
@@ -7,7 +8,24 @@ import { db } from "~/server/db";
 export default async function Home() {
     const posts = await db.query.posts.findMany();
 
-  return <main>{posts.map(posts => (<p> key={posts.name}</p>))}</main>;
-
+  return (
+    <main>
+    {posts.map((post) => (
+      <Post key={post.id} post={post} />
+    ))}
+    </main>
+  );
 }
 
+function Post({ post }: { post: {id: number, name: string | null}}) {
+    return <div className="flex gap-2 p-4  ">{post.name}
+            <form action={async () => {
+                "use server"
+
+                console.log("Deleting post", post.id);
+            }}>
+                <button className="bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded" type = "submit">X
+                </button>
+            </form>
+        </div>
+}

6433aaa7fed3f5bd52e30c3aabcd54aa57d4334e
Tushar Dahiya <shreyashsam@gmail.com>
2024-06-13 07:37:42 +0530

    yooooooooooooooooooo, it's not working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import "server-only";
+import { eq } from "drizzle-orm";
 import Link from "next/link";
 
 import { CreatePost } from "~/app/_components/create-post";
 import { api } from "~/trpc/server";
-import { db } from "~/server/db";
+i;d;mport { db } from "~/server/db";
+import { revalidatePath } from "next/cache";
 
 export default async function Home() {
     const posts = await db.query.posts.findMany();
@@ -18,14 +20,20 @@ export default async function Home() {
 }
 
 function Post({ post }: { post: {id: number, name: string | null}}) {
-    return <div className="flex gap-2 p-4  ">{post.name}
-            <form action={async () => {
-                "use server"
+    return ( 
+            <div className="flex gap-2 p-4  ">{post.name}
 
+            <form 
+                action={async () => {
+                "use server";
                 console.log("Deleting post", post.id);
+                await db.delete(posts).where(eq(posts.id, post.id));
+                revalidatePath("/");
             }}>
-                <button className="bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded" type = "submit">X
-                </button>
+            <button className="bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded" type = "submit">X
+            </button>
             </form>
-        </div>
+            </div>
+           )
 }
+
